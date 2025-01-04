7fbe527d00203c2ed3e8f8f31e8639fee1eca83c
Juan Pablo Benavente <84090568+JotaPeForty@users.noreply.github.com>
2025-01-03 21:04:29 -0300

    add google analytics

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,21 @@
 import '../styles/modules/globals.css'
-import {avenir} from '../styles/fonts.js'
+import { avenir } from '../styles/fonts.js'
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={avenir.className}>
       <head />
+      <!-- Google tag (gtag.js) -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-BTLDXSYRW1"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-BTLDXSYRW1');
+      </script>
       <body>
-      {children}
+        {children}
       </body>
     </html>
   )
